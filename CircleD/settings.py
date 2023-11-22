5238bb9487c602c89faf454880ae90438f5ffce6
dienzahraaa <dienzahra312@gmail.com>
2023-11-22 09:24:36 +0700

    deploy lagi

--- CircleD/settings.py
+++ CircleD/settings.py
@@ -52,6 +52,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ["https://dien-fitriani-tugas.pbp.cs.ui.ac.id"]
 
 CORS_ALLOW_HEADERS = [
     'accept',
