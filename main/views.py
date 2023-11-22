603725fc2459daeee065a8af24a5e32282143c8c
dienzahraaa <dienzahra312@gmail.com>
2023-11-22 11:01:11 +0700

    memperbaiki auth

--- main/views.py
+++ main/views.py
@@ -64,6 +64,7 @@ def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+@csrf_exempt
 def register(request):
     form = UserCreationForm()
 
@@ -76,13 +77,14 @@ def register(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
+@csrf_exempt
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            auth_login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main")) 
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
@@ -92,7 +94,7 @@ def login_user(request):
     return render(request, 'login.html', context)
 
 def logout_user(request):
-    logout(request)
+    auth_logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
@@ -161,46 +163,3 @@ def create_product_flutter(request):
     else:
         return JsonResponse({"status": "error"}, status=401)
     
-@csrf_exempt
-def logout(request):
-    username = request.user.username
-
-    try:
-        auth_logout(request)
-        return JsonResponse({
-            "username": username,
-            "status": True,
-            "message": "Logout berhasil!"
-        }, status=200)
-    except:
-        return JsonResponse({
-        "status": False,
-        "message": "Logout gagal."
-        }, status=401)
-    
-@csrf_exempt
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            auth_login(request, user)
-            # Status login sukses.
-            return JsonResponse({
-                "username": user.username,
-                "status": True,
-                "message": "Login sukses!"
-                # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
-            }, status=200)
-        else:
-            return JsonResponse({
-                "status": False,
-                "message": "Login gagal, akun dinonaktifkan."
-            }, status=401)
-
-    else:
-        return JsonResponse({
-            "status": False,
-            "message": "Login gagal, periksa kembali email atau kata sandi."
-        }, status=401)
